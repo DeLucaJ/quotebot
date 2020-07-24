c00f9e36be0839eb981bb34e3f9e90de9e72e7c0
Joseph DeLuca <delucaj96@gmail.com>
2020-07-23 20:20:06 -0700

    Cleans up Main and Changes getToken to getConfig

--- quotebot.go
+++ quotebot.go
@@ -21,7 +21,7 @@ const tfile string = "./data/token.txt"
 const uri = "mongodb://localhost:27017"
 
 // Retrieves the Bot Token
-func getToken(file string) string {
+func getConfig(file string) string {
 	t, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Println("Error reading token")
@@ -29,15 +29,26 @@ func getToken(file string) string {
 	return string(t)
 }
 
+func panicIfError(err error, message string) {
+	if err != nil {
+		fmt.Println(message + err.Error())
+		panic(err)
+	}
+}
+
 func main() {
 
 	// Store the application token
-	var token string = getToken(tfile)
+	var token string = getConfig(tfile)
 
 	// Connect to MongoDB
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel() // Defers the context cancel
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+
+	}
+
 	// Defer a disconnect from the database
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
@@ -47,23 +58,21 @@ func main() {
 
 	// Initialize the Discord Bot
 	ds, err := discordgo.New("Bot " + token)
-	if err != nil {
-		fmt.Println("Error creating Discord Session: " + err.Error())
-		return
-	}
-	defer ds.Close() // Defers a call to Close the Discord Session
+	panicIfError(err, "Error creating Discord Session: ")
 
 	// Define Handlers for discord events.
 
+	// Open Discord session
 	err = ds.Open()
-	if err != nil {
-		fmt.Println("Error opening Discord session: " + err.Error())
-		/* ds.Close()
-		return */
-	}
+	panicIfError(err, "Error opening Discord session: ")
+	// Defers a call to Close the Discord Session
+	defer ds.Close()
 
-	fmt.Println("Welcome to Quotebot X. Press CTRL+C to exit.")
+	// Creates Signal Interupt Channels
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
+
+	// Start Message
+	fmt.Println("Welcome to Quotebot X. Press CTRL+C to exit.")
 }
