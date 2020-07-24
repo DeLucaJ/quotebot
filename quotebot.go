52ba1a10bac97d4e78dd5c45fa7cfec6d6ee485d
Joseph DeLuca <delucaj96@gmail.com>
2020-07-23 20:12:22 -0700

    Adds MongoDB connection

--- quotebot.go
+++ quotebot.go
@@ -2,20 +2,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
-	// "go.mongodb.org/mongo-driver/mongo"
-	// "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	// "go.mongodb.org/mongo-driver/mongo/readpref"
 	// "github.com/DeLucaJ/quotebot/internal/types"
 )
 
 const tfile string = "./data/token.txt"
+const uri = "mongodb://localhost:27017"
 
 // Retrieves the Bot Token
 func getToken(file string) string {
@@ -31,14 +34,26 @@ func main() {
 	// Store the application token
 	var token string = getToken(tfile)
 
-	// Initialize the Bot
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel() // Defers the context cancel
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	// Defer a disconnect from the database
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Initialize the Discord Bot
 	ds, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("Error creating Discord Session: " + err.Error())
 		return
 	}
+	defer ds.Close() // Defers a call to Close the Discord Session
 
-	// Handlers for discord events.
+	// Define Handlers for discord events.
 
 	err = ds.Open()
 	if err != nil {
@@ -51,6 +66,4 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
-	ds.Close()
 }
