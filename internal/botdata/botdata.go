c6c7e7e0cde571c9a0c9be39a856c0e21c0bef0e
Joseph DeLuca <delucaj96@gmail.com>
2020-09-15 12:23:44 -0700

    Adds support for a config.json file

--- internal/botdata/botdata.go
+++ internal/botdata/botdata.go
@@ -10,39 +10,72 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const uri = "mongodb://localhost:27017"
+// Manager - struct that holds the data necessary to manage the database
+type Manager struct {
+	client   *mongo.Client
+	ctx      context.Context
+	cancel   context.CancelFunc
+	database *mongo.Database
+	guildcol *mongo.Collection
+	usercol  *mongo.Collection
+	quotecol *mongo.Collection
+}
 
-var guildCollection mongo.Collection
-var userCollection mongo.Collection
-var quoteCollection mongo.Collection
+// Shutdown - Ends the connection to the database and cleans the context
+func (bm Manager) Shutdown() {
+	cancel := bm.cancel
+	client := bm.client
+	ctx := bm.ctx
 
-func init() {
-
-	fmt.Println("Initializeing Mongo Client")
-
-	// Connect to MongoDB
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel() // Defers the context cancel
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	if err != nil {
-		fmt.Println("Error connecting to mongo client: " + err.Error())
-		log.Fatal(err)
-	}
-
-	// Defer a disconnect from the database
+	defer cancel()
 	defer func() {
+		err := client.Disconnect(ctx)
 		if err != nil {
 			fmt.Println("Error disconnecting from mongo client: " + err.Error())
 			log.Fatal(err)
 		}
 	}()
+}
+
+// Start - Starts the boss and initializes the database connection
+// 	uri string: the uri for the database from config.json
+func Start(uri string) Manager {
+	fmt.Println("Initializeing Mongo Client")
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel() // Defers the context cancel
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		// defers closing functions after error
+		defer cancel()
+		defer func() {
+			err := client.Disconnect(ctx)
+			if err != nil {
+				fmt.Println("Error disconnecting from mongo client: " + err.Error())
+				log.Fatal(err)
+			}
+		}()
+
+		fmt.Println("Error connecting to mongo client: " + err.Error())
+		log.Fatal(err)
+	}
 
 	// Initialize the Database
-	database := client.Database("quotedb")
-	guildCollection := database.Collection("guilds")
-	userCollection := database.Collection("users")
-	quoteCollection := database.Collection("quotes")
+	db := client.Database("quotebotx")
+	gc := db.Collection("guilds")
+	uc := db.Collection("users")
+	qc := db.Collection("quotes")
 
-	fmt.Println(guildCollection.Name, userCollection.Name, quoteCollection.Name)
-
+	// initializes the singleton botmanager
+	return Manager{
+		client:   client,
+		ctx:      ctx,
+		cancel:   cancel,
+		database: db,
+		guildcol: gc,
+		usercol:  uc,
+		quotecol: qc,
+	}
 }
--- quotebot.go
+++ quotebot.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,12 +10,16 @@ import (
 	"os/signal"
 	"syscall"
 
-	// "json"
-
+	"github.com/DeLucaJ/quotebot/internal/botdata"
 	"github.com/bwmarrin/discordgo"
 )
 
-const tfile string = "./data/token.txt"
+const configfile string = "./config.json"
+
+type config struct {
+	Token string
+	DBuri string
+}
 
 // Used for general error checking and panicing
 func checkError(err error, message string) {
@@ -26,19 +31,32 @@ func checkError(err error, message string) {
 
 // Retrieves the Bot Token
 // Will eventually get the JSON configuration for quotebot
-func getConfig(file string) string {
-	t, err := ioutil.ReadFile(file)
+func getConfig(file string) config {
+	// read the fildata of configfile
+	filedata, err := ioutil.ReadFile(file)
 	checkError(err, "Error reading discord token: ")
-	return string(t)
+
+	//turn config.json into a config struct
+	var configuration config
+	err = json.Unmarshal(filedata, &configuration)
+	if err != nil {
+		fmt.Println("Error unmarshalling json: ", err)
+	}
+
+	return configuration
 }
 
 func main() {
+	// Store the application configuration
+	botconfig := getConfig(configfile)
 
-	// Store the application token
-	var token string = getConfig(tfile)
+	// Starts the data manager for the bot
+	dataManager := botdata.Start(botconfig.DBuri)
+	// defers the graceful shutdown of the data manager
+	defer dataManager.Shutdown()
 
 	// Initialize the Discord Bot
-	ds, err := discordgo.New("Bot " + token)
+	ds, err := discordgo.New("Bot " + botconfig.Token)
 	checkError(err, "Error creating Discord Session: ")
 
 	// Define Handlers for discord events.
@@ -49,11 +67,11 @@ func main() {
 	// Defers a call to Close the Discord Session
 	defer ds.Close()
 
+	// Start Message
+	fmt.Println("Welcome to Quotebot X. Press CTRL+C to exit.")
+
 	// Creates Signal Interupt Channels
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
-	// Start Message
-	fmt.Println("Welcome to Quotebot X. Press CTRL+C to exit.")
 }
