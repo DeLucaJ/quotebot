b40494a771afcb7d1f3a73d3858303c6f9601205
DeLucaJ <delucaj96@gmail.com>
2023-02-20 13:39:19 -0800

    Revert "Changes a call to session.User to session.GuildMember to try and get a more recent color for a user."
    This reverts commit 6cf5798d6d7be8c3cc4892c4e0d787d0b7467bd5.

--- responses.go
+++ responses.go
@@ -69,15 +69,15 @@ func quoteToEmbed(session *discordgo.Session, quote data.Quote) *discordgo.Messa
 		Text: fmt.Sprintf("Submitted by %s", quote.Submitter.Name),
 	}
 
-	speakerInfo, _ := session.GuildMember(quote.Guild.DiscordID, quote.Speaker.DiscordID)
+	speakerInfo, _ := session.User(quote.Speaker.DiscordID)
 
 	thumbnail := discordgo.MessageEmbedThumbnail{
-		URL: speakerInfo.User.AvatarURL(""),
+		URL: speakerInfo.AvatarURL(""),
 	}
 
 	embed := discordgo.MessageEmbed{
 		Type:        discordgo.EmbedTypeRich,
-		Color:       speakerInfo.User.AccentColor,
+		Color:       speakerInfo.AccentColor,
 		Title:       fmt.Sprintf("%s", quote.Speaker.Name),
 		Description: fmt.Sprintf("\"%s\"", quote.Content),
 		Footer:      &footer,
